65cdafd80bce0898459f8d0c91b30ae33b0676ae
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-22 18:49:17 +0530

    Added code to part time EmpWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,21 +3,31 @@ package assignments.EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
 
         int empHour = 0;
         int empWage = 0;
-        double attendanceCheck  =Math.floor(Math.random() * 10) % 2;
-        if(attendanceCheck==1){
-            System.out.println("Employee is present");
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+
+        if(empCheck == IS_FULL_TIME)
+        {
+            System.out.println("Employee is Present & Doing Full Time");
             empHour = 8 ;
         }
-        else {
-            System.out.println("Employee is absent");
+        else if(empCheck == IS_PART_TIME)
+        {
+            System.out.println("Employee is Present & Doing Part Time");
+            empHour = 4;
+        }
+        else
+        {
+            System.out.println("Employee is Absent");
             empHour = 0;
         }
         empWage = empHour * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage is :" + empWage);
-
     }
 }
