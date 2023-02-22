88bf28369aba28873cb90f0ecfd793869a5abb60
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-22 19:11:33 +0530

    Refactored code by using switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,33 +1,39 @@
 package assignments.EmployeeWage;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program");
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
+    public static void main(String args[])
+    {
+
+        System.out.println("Welcome to the Employee Wage Computation Program");
 
         int empHour = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        if(empCheck == IS_FULL_TIME)
+        switch(empCheck)
         {
-            System.out.println("Employee is Present & Doing Full Time");
-            empHour = 8 ;
-        }
-        else if(empCheck == IS_PART_TIME)
-        {
-            System.out.println("Employee is Present & Doing Part Time");
-            empHour = 4;
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
-            empHour = 0;
+            case IS_FULL_TIME:
+                System.out.println("Employee is Present & Doing Full Time");
+                empHour = 8 ;
+                break;
+
+            case IS_PART_TIME:
+                System.out.println("Employee is Present & Doing Part Time");
+                empHour = 4;
+                break;
+
+            default:
+                System.out.println("Employee is Absent");
+                empHour = 0;
+
         }
         empWage = empHour * EMP_RATE_PER_HOUR;
         System.out.println("Employee Wage is :" + empWage);
+
     }
 }
