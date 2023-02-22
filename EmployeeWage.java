6a1641ceecded3ce7abfe8cc31f759be097b4c31
shraddha0930 <114004352+shraddha0930@users.noreply.github.com>
2023-02-22 18:13:30 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,7 @@ package assignments.EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
-        int attendanceCheck=(int)(Math.random()*10)%2;
+        double attendanceCheck  =Math.floor(Math.random() * 10) % 2;
         if(attendanceCheck==1)
             System.out.println("Employee is present");
         else
