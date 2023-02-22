9c2054c936c9ab0557c8d4eb5c7d4ace80e53de0
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-22 17:45:25 +0530

    Added Welcome message

---
+++ EmployeeWage.java
@@ -0,0 +1,8 @@
+package assignments.EmployeeWage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+
+    }
+}
