de0846b98a0f9a6ece2b26b4372b077b0935574e
shraddha0930 <shraddhaingole11@gmail.com>
2023-02-22 18:21:25 +0530

    Added code to calculate daily EmpWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,11 +3,21 @@ package assignments.EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Program");
-        int attendanceCheck=(int)(Math.random()*10)%2;
-        if(attendanceCheck==1)
+        int EMP_RATE_PER_HOUR = 20;
+
+        int empHour = 0;
+        int empWage = 0;
+        double attendanceCheck  =Math.floor(Math.random() * 10) % 2;
+        if(attendanceCheck==1){
             System.out.println("Employee is present");
-        else
+            empHour = 8 ;
+        }
+        else {
             System.out.println("Employee is absent");
+            empHour = 0;
+        }
+        empWage = empHour * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage is :" + empWage);
 
     }
 }
