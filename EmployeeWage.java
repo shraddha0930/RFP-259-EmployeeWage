2db782edb6e5e307fc9698e1bf13d0ebd59eb6ce
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-05 23:54:39 +0530

    Added:code to store the daily wage along with total wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,38 +6,54 @@ public class EmployeeWage implements InterfaceEmpwage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-    private int numofCompany = 0;                 // Take a Varible for Starting Array with 0th index
     private ArrayList<CompanyEmpwage> companyEmpArrayList;
-    public EmployeeWage() {
+
+
+    public EmployeeWage()
+    {
         companyEmpArrayList = new ArrayList<CompanyEmpwage>();  // Initallizing ArrayList in Default Constructor
     }
 
-    public void addCompanyEmpwage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+    public void addCompanyEmpwage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+    {
         CompanyEmpwage companyEmpwage = new CompanyEmpwage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
         companyEmpArrayList.add(companyEmpwage);
     }
-
-    public void computeEmpWage() {
-        for (int i = 0; i < numofCompany; i++) {
+    public void computeEmpWage()
+    {
+        for (int i = 0; i<companyEmpArrayList.size();i++)
+        {
             CompanyEmpwage companyEmpWage = companyEmpArrayList.get(i);
             companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
-            System.out.println(companyEmpWage.company+" Total Wage is: "+ companyEmpWage.totalEmpWage);
+            System.out.println("Company Name : "+ companyEmpWage.company);
+            DisplayDailyWageforCompany(companyEmpWage);					// Calling a Method with passing a One Parameter and Show Total Wage
+            System.out.println("Total Wage is: "+ companyEmpWage.totalEmpWage);
         }
     }
+    public void DisplayDailyWageforCompany(CompanyEmpwage companyEmpwage)
+    {
+        for(int i=0; i<companyEmpwage.empDailyWage.size();i++)
+        {
+            int day = i + 1;
+            System.out.println("Daily Wage For Day" + day +":" + companyEmpwage.empDailyWage.get(i));      // Displaying a Daily Wage for Day
+        }
+    }
+    public int computeEmpWage(CompanyEmpwage companyEmpWage)
+    {
 
-    public int computeEmpWage(CompanyEmpwage companyEmpWage) {
+        int totalEmpWage = 0,totalWorkingDays = 0,totalEmpHrs = 0;
 
-        int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
-
-        while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
+        while(totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+        {
             int empHour = 0;
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-            switch (empCheck) {
+            switch(empCheck)
+            {
                 case IS_FULL_TIME:
                     System.out.println("Employee is Present & Doing Full Time");
-                    empHour = 8;
+                    empHour = 8 ;
                     break;
 
                 case IS_PART_TIME:
@@ -50,17 +66,18 @@ public class EmployeeWage implements InterfaceEmpwage {
                     empHour = 0;
 
             }
-            totalEmpHrs += empHour;            // Calculting a EmpHour and Printing a Emp Hour
-            System.out.println("Day-" + totalWorkingDays + " Employee Hour is :" + empHour);
+            totalEmpHrs += empHour;  // Calculting a EmpHour and Printing a Emp Hour
+            System.out.println("Day-"+ totalWorkingDays + " Employee Hour is :" + empHour);
         }
-        return totalEmpHrs * companyEmpWage.empRatePerHour;    // Multiply Total EmpHrs with Rate Per Hour and Returning the Value
+        return totalEmpHrs * companyEmpWage.empRatePerHour;  // Multiply Total EmpHrs with Rate Per Hour and Returning the Value
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         System.out.println("Welcome to the Employee Wage Computation Program");
-        EmployeeWage emp = new EmployeeWage();                    // Creating a Object of Class
-        emp.addCompanyEmpwage("DMart", 20, 2, 10);
-        emp.addCompanyEmpwage("Reliance", 10, 4, 20);            // Calling a addCompany Method and Add One By One Company To The Array
-        emp.computeEmpWage();                        // Calling a Method
+        EmployeeWage emp = new EmployeeWage();  					// Creating a Object of Class
+        emp.addCompanyEmpwage("DMart",20, 2, 10);
+        emp.addCompanyEmpwage("Reliance",10, 4, 20);			// Calling a addCompany Method and Add One By One Company To The ArrayList
+        emp.computeEmpWage();						// Calling a Method
     }
 }
