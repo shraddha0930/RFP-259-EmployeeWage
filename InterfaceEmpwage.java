72307c04e169cd4d4b8a2c51bd09f9a274a1c850
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-05 23:27:48 +0530

    Refactor:code to manage empwage of multiple companies using interface

---
+++ InterfaceEmpwage.java
@@ -0,0 +1,9 @@
+package assignments.EmployeeWage;
+
+public interface InterfaceEmpwage {
+	
+	public void addCompanyEmpwage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);   // Declearing addCompany Method in Interface
+	
+	public void computeEmpWage(); 		// Declearing computeEmpWage Method in Interface
+
+}
